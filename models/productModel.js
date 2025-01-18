78a6004366dc97452cba8c92da6b9a458b3303c6
Bittu75469 <bittukr4715@gmail.com>
2025-01-18 09:14:14 +0530

    chnages

--- models/productModel.js
+++ models/productModel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const ProductSchema = new mongoose.Schema(
   {
-    brandname: {type: String, required: true},
+    brandnameee: { type: String, required: true },
     name: { type: String, required: true },
     description: { type: String, required: true },
     price: { type: Number, required: true },
