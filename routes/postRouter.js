fdf7fe2b003a33a97194640b7392ddedd92bcf9c
Mihir Kumar Mishra <mkm20170@gmail.com>
2025-02-19 11:31:02 +0530

    Changes

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -3,13 +3,13 @@ const auth = require("../middleware/auth");
 const postCtrl = require("../controllers/postCtrl");
 const upload = require("../middleware/uploadMiddleware");
 
+router.post("/posts", auth, upload, postCtrl.createPost);
+router.get("/posts", postCtrl.getPosts);
 
-router.post("/posts", auth, upload, postCtrl.createPost); 
-router.get('/posts', postCtrl.getPosts);
-
-router.route("/post/:id")
+router
+  .route("/post/:id")
   .patch(auth, postCtrl.updatePost)
-  .get(auth, postCtrl.getPost)
+  .get( postCtrl.getPost)
   .delete(auth, postCtrl.deletePost);
 
 router.patch("/post/:id/like", auth, postCtrl.likePost);
@@ -24,8 +24,5 @@ router.get("/post_discover", auth, postCtrl.getPostDiscover);
 router.patch("/savePost/:id", auth, postCtrl.savePost);
 router.patch("/unSavePost/:id", auth, postCtrl.unSavePost);
 router.get("/getSavePosts", auth, postCtrl.getSavePost);
-
-
-
 
 module.exports = router;
