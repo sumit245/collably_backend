3f9f1944280ac7aaf14a311559f69c5bb30a22cf
sumitranjan245@gmail.com <sumitranjan245@gmail.com>
2025-04-09 15:01:12 +0530

    message

--- routes/brandsApi.js
+++ routes/brandsApi.js
@@ -1,43 +1,30 @@
-const express = require("express");
-const path = require("path");
-const brandController = require("../controllers/brandController");
-const upload = require("../middleware/uploadMiddleware");
-const mongoose = require("mongoose");
+/**
+ * Express router for brand-related API endpoints
+ * 
+ * Provides routes for brand operations including:
+ * - Creating a new brand
+ * - Brand login
+ * - Retrieving all brands
+ * - Retrieving a specific brand by ID
+ * - Updating a brand
+ * - Deleting a brand
+ * 
+ * Uses upload middleware for handling file uploads related to brands
+ * Depends on brandController for handling the logic of each route
+ * 
+ * @module brandsApi
+ */
 
-const router = express.Router();
-
-const isValidObjectId = (id) => mongoose.Types.ObjectId.isValid(id);
+const router = require("express").Router()
+const brandController = require("../controllers/brandController")
+const upload = require("../middleware/uploadMiddleware")
 
 
 router.post("/createbrand", upload('brands'), brandController.createBrand);
-
-
 router.post("/brandlogin", brandController.login);
 router.get("/brands", brandController.getAllBrands);
 router.get("/brand/:id", brandController.getBrandById);
-
-
-router.put(
-  "/brands/:id",
-  upload('brands'),
-  (req, res, next) => {
-    const { id } = req.params;
-    if (!isValidObjectId(id)) {
-      return res.status(400).json({ message: "Invalid ObjectId format" });
-    }
-    next();
-  },
-  brandController.updateBrand
-);
-
-router.delete("/brand/:id", (req, res, next) => {
-  const { id } = req.params;
-  if (!isValidObjectId(id)) {
-    return res.status(400).json({ message: "Invalid ObjectId format" });
-  }
-  next();
-},
-  brandController.deleteBrand
-);
+router.put("/brands/:id", upload('brands'), brandController.updateBrand);
+router.delete("/brand/:id", brandController.deleteBrand);
 
 module.exports = router;
