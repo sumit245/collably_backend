a90bf74723a381bcfd37e24588ac6f0d7be642fb
dheerajyadav1468 <dheerajyadav1468@gmail.com>
2025-04-10 15:31:53 +0530

    latest testing deployement 3

--- routes/blogRouter.js
+++ routes/blogRouter.js
@@ -7,10 +7,7 @@ const uploadMiddleware = require("../middleware/uploadMiddleware");
 
 router.post("/upload", auth, uploadMiddleware, blogCtrl.uploadBlog); 
 router.put("/update_blog/:id", auth, uploadMiddleware, blogCtrl.updateBlog); 
-router.get("/view_blogs", (req, res) => {
-    return res.status(200).json({ message: "✅ Blog route working!" });
-  });
-  
+router.get("/view_blogs", blogCtrl.viewBlogs); 
 router.delete("/delete_blogs/:id", auth, blogCtrl.deleteBlog);
 router.get("/view_blogs/:id", blogCtrl.getBlogById); 
 module.exports = router;
